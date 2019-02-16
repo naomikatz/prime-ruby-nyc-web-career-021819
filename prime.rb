5a427d964c2c92dcf4cbcdec592c99b84319d5f5
Naomi Katz <naykatz@gmail.com>
2019-02-16 03:08:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1  
+    return false
+  end
+  (2...number).each do |n|
+    if number % n == 0 
+      return false 
+    end
+  end
+  return true 
+end
+
+
+    
+  
+  
+  
